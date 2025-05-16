fe594caf61fb3ff32db75cd87f09b6d2a3521c5d
rustam-AIresearch <rustam24052003@gmail.com>
2025-05-16 01:06:42 +0100

    first commit

---
+++ src/data/generated/bib.js
@@ -0,0 +1,118 @@
+﻿define({ entries : {
+    "GonzalesHuisa2023EMG": {
+        "author": "Gonzales-Huisa, O.~A. and Oshiro, G. and Abarca, V.~E. and Chavez-Echajaya, J.~G. and Elias, D.~A.",
+        "doi": "10.3390/prosthesis5040085",
+        "keywords": "type:hybrid",
+        "number": "4",
+        "pages": "85",
+        "series": "Prosthesis",
+        "title": "EMG and IMU Data Fusion for Locomotion Mode Classification in Transtibial Amputees",
+        "type": "article",
+        "volume": "5",
+        "year": "2023"
+    },
+    "McLeod2016Using": {
+        "author": "McLeod, A. and others",
+        "doi": "10.1016/j.apmr.2015.08.435",
+        "keywords": "type:comparative",
+        "number": "2",
+        "pages": "224--231",
+        "series": "Archives of Physical Medicine and Rehabilitation",
+        "title": "Using Wearable Sensors and Machine Learning Models to Separate Functional Upper Extremity Use from Walking-Associated Arm Movements",
+        "type": "article",
+        "volume": "97",
+        "year": "2016"
+    },
+    "Mekruksavanich2022Deep": {
+        "author": "Mekruksavanich, S. and Jantawong, P. and Jitpattanakul, A.",
+        "doi": "10.1016/j.procs.2022.11.220",
+        "keywords": "type:hybrid",
+        "pages": "621--627",
+        "series": "Procedia Computer Science",
+        "title": "A Deep Learning\u2013Based Model for Human Activity Recognition Using Biosensors Embedded into a Smart Knee Bandage",
+        "type": "article",
+        "volume": "214",
+        "year": "2022"
+    },
+    "Moawad2025New": {
+        "author": "Moawad, A. and El-Baz, A. and others",
+        "doi": "10.3390/asi8020053",
+        "keywords": "type:comparative",
+        "number": "2",
+        "pages": "53",
+        "series": "Applied System Innovation",
+        "title": "New Framework for Human Activity Recognition for Wearable Gait Rehabilitation Systems",
+        "type": "article",
+        "volume": "8",
+        "year": "2025"
+    },
+    "Moghadam2023Comparison": {
+        "author": "Moghadam, S.~M. and Yeung, T. and Choisne, J.",
+        "doi": "10.1038/s41598-023-31906-z",
+        "keywords": "type:comparative",
+        "number": "1",
+        "pages": "31906",
+        "series": "Scientific Reports",
+        "title": "A Comparison of Machine Learning Models\u2019 Accuracy in Predicting Lower-Limb Joints\u2019 Kinematics, Kinetics, and Muscle Forces from Wearable Sensors",
+        "type": "article",
+        "volume": "13",
+        "year": "2023"
+    },
+    "Regterschot2021Wearable": {
+        "author": "Regterschot, G.~R. and Ribbers, G.~M. and Bussmann, J.~B.",
+        "doi": "10.3390/s21144744",
+        "keywords": "type:theoretical",
+        "number": "14",
+        "pages": "4744",
+        "series": "Sensors",
+        "title": "Wearable Movement Sensors for Rehabilitation: From Technology to Clinical Practice",
+        "type": "article",
+        "volume": "21",
+        "year": "2021"
+    },
+    "Siviero2024Remote": {
+        "author": "Siviero, I. and others",
+        "doi": "10.1109/embc53108.2024.10782268",
+        "keywords": "type:hybrid",
+        "pages": "1--4",
+        "series": "Proceedings of the 46th Annual International Conference of the IEEE Engineering in Medicine and Biology Society (EMBC)",
+        "title": "Remote Motor Rehabilitation: EMG\u2013IMU Based Deep Learning Model Improves the Estimate of Wrist Kinematics",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "Vasconez2022Hand": {
+        "author": "V\u00e1sconez, J.~P. and Castellini, C. and Castellini, C. and others",
+        "doi": "10.3390/s22249613",
+        "keywords": "type:comparative",
+        "number": "24",
+        "pages": "9613",
+        "series": "Sensors",
+        "title": "Hand Gesture Recognition Using EMG\u2013IMU Signals and Deep Q-Networks",
+        "type": "article",
+        "volume": "22",
+        "year": "2022"
+    },
+    "Xiong2021Deep": {
+        "author": "Xiong, D. and Zhang, D. and Zhao, X. and Zhao, Y.",
+        "doi": "10.1109/JAS.2021.1003865",
+        "keywords": "type:theoretical",
+        "number": "3",
+        "pages": "512--533",
+        "series": "IEEE/CAA Journal of Automatica Sinica",
+        "title": "Deep Learning for EMG-based Human\u2013Machine Interaction: A Review",
+        "type": "article",
+        "volume": "8",
+        "year": "2021"
+    },
+    "Zhou2020Accurate": {
+        "author": "Zhou, B. and Peng, Q. and others",
+        "doi": "10.1016/j.cmpb.2020.105486",
+        "keywords": "type:comparative",
+        "pages": "105486",
+        "series": "Computer Methods and Programs in Biomedicine",
+        "title": "Accurate Recognition of Lower Limb Ambulation Mode Based on Surface Electromyography and Motion Data Using Machine Learning",
+        "type": "article",
+        "volume": "193",
+        "year": "2020"
+    }
+}});
